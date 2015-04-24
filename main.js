71d31163b32d774cba15d1f5caac602f9649f3ea
dgd8 <hillis.cs.calvin.edu>
2015-04-23 22:50:59 -0400

    * added planets and raycasting picking

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ $(document).ready(function() {
 
 var main = {};
 
+function randInt(a,b){
+    return a + parseInt(Math.random()*(b-a+1));
+}
+
 main.Init = function() {
     main.width    = $("#ruler")[0].clientWidth;
     main.height   = main.width * 9 / 16;
@@ -12,20 +16,42 @@ main.Init = function() {
         antialias: true
     });
     main.renderer.setSize(main.width, main.height);
-    main.camera = new THREE.PerspectiveCamera(45, main.width / main.height, 0.1, 1000);
-    main.camera.position.z = 5;
+    main.camera = new THREE.PerspectiveCamera(45, main.width / main.height, 1, 15000);
+    main.camera.position.z = 30;
     main.scene = new THREE.Scene();
     main.clock = new THREE.Clock();
+    main.projector = new THREE.Projector();
+    main.raycaster = new THREE.Raycaster();
+    main.mouse = new THREE.Vector3();
 
     $("#btnFullscr").click(main.OnFullscr);
     $(window).resize($.throttle(300, main.OnResize));
-    phys.Init(main.scene);
+    document.addEventListener( 'mousemove', main.OnMouseMove, false );
     visu.Init(main.scene);
-    main.Render();
+    phys.Init(main.scene.children[0]);
+    main.Render();  
 }
 
 main.OnFullscr = function() {
     $("#canvas").fullScreen(true);
+}
+
+main.OnMouseMove = function(event) {
+    var offset = $("#canvas").offset();
+    main.mouse.x = (event.pageX - offset.left)/main.width *2 - 1;
+    main.mouse.y = -(event.pageY - offset.top)/main.height *2 + 1;
+    
+    main.scene.children[1].children.forEach(function(planet){
+        planet.material.color.setRGB(0,0,255);
+    });
+    
+    main.mouse.unproject(main.camera);
+    main.raycaster.set(main.camera.position, main.mouse.sub(main.camera.position).normalize());
+    var intersects = main.raycaster.intersectObjects(main.scene.children[1].children);
+    
+    intersects.forEach(function(intersection){
+        intersection.object.material.color.setRGB(0,255,0);
+    });
 }
 
 main.OnResize = function() {
--- visu.js
+++ visu.js
@@ -3,12 +3,51 @@ var visu = {};
 visu.Init = function(scene) {
     var geom = new THREE.BoxGeometry(0.1, 0.1, 0.1);
     var mat  = new THREE.MeshLambertMaterial({color: 0x00ff00});
+    var cubes = new THREE.Object3D();
+    scene.add(cubes); // scene.children[0]
     for (var i = 0; i < 10; ++i) {
         var cube = new THREE.Mesh(geom, mat);
         cube.position.set(Math.random()*4-2, Math.random()*4-2, Math.random()*4-2);
         cube.rotation.z = Math.random() * 3;
-        scene.add(cube);
+        cubes.add(cube);
+    }      
+    planets = new THREE.Object3D();
+    scene.add(planets); // scene.children[1]
+
+    for(var i=0; i<10; i++){
+        var planetSize = 0.5;
+        var planetColor = "red";
+        var geometry = new THREE.SphereGeometry( planetSize, 32, 32 );
+        var material = new THREE.MeshBasicMaterial( {color: planetColor} );
+        var planet = new THREE.Mesh(geometry,material);
+        planet.planetSize = planetSize;
+        planet.planetColor = planetColor;
+        planet.position.set(randInt(-5,5),randInt(-5,5),randInt(-5,5));
+        //planet.id = i;
+        planets.add(planet);
+
+        /*var textGeom = new THREE.TextGeometry(randInt(1,50), {
+            size: 10,
+            height: 1
+        });
+        var textMat = new THREE.MeshBasicMaterial( {color: "white"} );
+        mesh = new THREE.Mesh( textGeom, textMat );
+        mesh.position.set(planet.position.x-5,planet.position.y+15,planet.position.z)
+        scene.add( mesh );*/
     }
+
+    var directions  = ["xpos", "xneg", "ypos", "yneg", "zneg", "zpos"];
+    var skyGeometry = new THREE.BoxGeometry( 10000, 10000, 10000 );    
+    var materialArray = [];
+    for (var i = 0; i < 6; i++)
+        materialArray.push( new THREE.MeshBasicMaterial({
+            map: THREE.ImageUtils.loadTexture( "img/" + directions[i] + ".png" ),
+            side: THREE.BackSide
+        }));
+    var skyMaterial = new THREE.MeshFaceMaterial( materialArray );
+    var skyBox = new THREE.Mesh( skyGeometry, skyMaterial );
+    scene.add( skyBox );
+    
     scene.add(new THREE.AmbientLight(0x222222));
     var light = new THREE.PointLight(0xffffff);
     light.position.set(10, 10, 10);
